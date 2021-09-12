11910b8d0f858dd3e3829e15fb787a10146e2c81
Xiang Cao <cxhawk@gmail.com>
2021-09-11 20:13:24 -0700

    add icon

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	pluginOptions: {
+		electronBuilder: {
+			builderOptions: {
+				// options placed here will be merged with default configuration and passed to electron-builder
+				"appId": "com.maizesoft.livesubs",
+				"productName": "LiveSubs"
+			}
+		}
+	}
+}
