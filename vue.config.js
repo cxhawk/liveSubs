1f671e865e2f8ce68d60d474b81457ef13486e90
Xiang Cao <cxhawk@gmail.com>
2021-09-18 09:10:45 -0700

    notarization

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,12 @@ module.exports = {
 				// options placed here will be merged with default configuration and passed to electron-builder
 				"appId": "com.maizesoft.livesubs",
 				"productName": "LiveSubs",
+				"afterSign": "provisioning/notarize.js",
+				mac: {
+					"hardenedRuntime": true,
+					"provisioningProfile": "provisioning/LiveSubsDevIDApp.provisionprofile",
+					"entitlements": "provisioning/mac.entitlement",
+				},
 				linux: {
 					target: {
 						target: 'appimage',
