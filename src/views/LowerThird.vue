1ff106249ba60b7d06e0090dfa73f54599e7b1c4
Xiang Cao <cxhawk@gmail.com>
2023-06-18 01:57:20 -0700

    fix issue #6

--- src/views/LowerThird.vue
+++ src/views/LowerThird.vue
@@ -99,7 +99,7 @@
       <el-input
         type="textarea"
         :autosize="{ minRows: 5, maxRows: 20 }"
-        placeholder="one line per title and description (use space between title and description)"
+        placeholder="one line per title and description (use - between title and description)"
         v-model="importText"
       ></el-input>
       <span slot="footer" class="dialog-footer">
@@ -182,7 +182,7 @@ export default {
         if (lines.length) {
           let newItems = [];
           for (const line of lines) {
-            const elements = line.split(" ");
+            const elements = line.split("-");
             if (elements.length >= 2) {
               newItems.push({ id: nanoid(), title: elements[0].trim(), description: elements[1].trim() });
             } else {
@@ -229,7 +229,7 @@ export default {
 				this.currentTemplate.items.splice(this.insertingIndex, 0, this.editingItem);
 				this.insertingIndex = 0;
 			}
-      this.$store.dispatch("showLowerThird", this.currentLowerThirdId); // update in case text changed
+      this.$store.dispatch("showLowerThird", {templateId: this.$route.params.id, itemId: this.currentLowerThirdId}); // update in case text changed
 			this.$store.dispatch("save");
 			this.editingItem = null;
 		},
